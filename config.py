a5bd014bc4adf12b8ce89c405242961b5a7c7715
girlypost123-hub <girlypost123@gmail.com>
2025-12-11 13:52:59 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+OWNER_ID = 5994305183  # Replace with your Telegram user ID
+OFFICIAL_GROUP_LINK = "https://t.me/CodeFlix_Xx"  # Replace with your group link
+AUTHORIZED_PRIVATE_USERS = set()  # Start empty, or preload user IDs
+AUTHORIZED_CHATS = set((-1003232934009,)) # Start empty, or preload chat IDs
+ADMIN_IDS = [5994305183]  # Replace with your real Telegram user ID
